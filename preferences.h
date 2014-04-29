a0a5e8e2fafd390e5b4a3314e0d3a99aa24191e4
Guido Aulisi <guido.aulisi@gmail.com>
2014-04-29 21:54:48 +0200

    Add license header

--- preferences.h
+++ preferences.h
@@ -7,12 +7,12 @@
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * pg800 is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -20,10 +20,7 @@
 #ifndef _PREFERENCES_H_
 #define _PREFERENCES_H_
 
-#include <gtkmm/dialog.h>
-#include <gtkmm/comboboxtext.h>
-#include <gtkmm/spinbutton.h>
-#include <gtkmm/box.h>
+#include <gtkmm/gtkmm.h>
 #include <vector>
 #include <string>
 
