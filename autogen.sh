c17a9ec524a217fdc8cad3cff70ac19f2abb6272
Guido Aulisi <guido.aulisi@gmail.com>
2014-09-02 16:53:26 +0200

    Build with new 128 pixel icon

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
-inkscape --export-png=icons/pg800.png icons/pg800-48x48.svg
+mkdir -p icons/{48,128}
+inkscape --export-png=icons/48/pg800.png icons/pg800-48x48.svg
+inkscape --export-png=icons/128/pg800.png icons/pg800-128x128.svg
 autoreconf -if
